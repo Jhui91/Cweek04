3d1cb540fdfb1bf1f67eda5a5b71af97eea87a92
Jhui91 <jinso0911@naver.com>
2025-04-24 09:10:23 +0900

    Cweek06a

--- cweek05a/uicomponents/MainScreen.kt
+++ cweek05a/uicomponents/MainScreen.kt
@@ -1,122 +1,47 @@
 package com.example.cweek05a.uicomponents
 
 import android.content.res.Configuration
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withRunningRecomposer
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.cweek05a.model.ImageData
+import com.example.cweek05a.model.ImageUri
+import com.example.cweek05a.R
 import com.example.cweek05a.model.ButtonType
-import com.example.cweek05a.uiexamples.ScrollToTopButton
 import com.example.cweek05a.viewmodel.ImageViewModel
-import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
-    imageViewModel: ImageViewModel = viewModel()
+    imageViewModel:ImageViewModel = viewModel()
 ) {
     val imageList = imageViewModel.imageList
     val orientation = LocalConfiguration.current.orientation
-    val state = rememberLazyListState()
-    val scope = rememberCoroutineScope()
-
-    val showButton by remember {
-        derivedStateOf {
-            state.firstVisibleItemIndex > 0
+    if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ImageList(imageList = imageList)
         }
-    }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            LazyColumn(
-                state = state,
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                contentPadding = PaddingValues(10.dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-            ) {
-                itemsIndexed(imageList) { index, item ->
-                    when (item.buttonType) {
-                        ButtonType.ICON -> {
-                            ImageWithButton(image = item.image) {
-                                ButtonWithIcon(likes = item.likes) {
-                                    imageList[index] = item.copy(likes = item.likes + 1)
-                                }
-                            }
-                        }
-
-                        ButtonType.BADGE -> {
-                            ImageWithButton(image = item.image) {
-                                ButtonWithBadge(likes = item.likes) {
-                                    imageList[index] = item.copy(likes = item.likes + 1)
-                                }
-                            }
-                        }
-
-                        ButtonType.EMOJI -> {
-                            ImageWithButton(image = item.image) {
-                                ButtonWithEmoji(
-                                    likes = item.likes,
-                                    dislikes = item.dislikes,
-                                    onClickLikes = {
-                                        imageList[index] = item.copy(likes = item.likes + 1)
-                                    },
-                                    onClickDisLikes = {
-                                        imageList[index] = item.copy(dislikes = item.dislikes + 1)
-                                    }
-                                )
-                            }
-                        }
-                    }
-                }
-//                item() {
-//                    ImageList(imageList = imageList)
-//                }
-            }
-            AnimatedVisibility(visible = showButton) {
-                ScrollToTopButton {
-                    scope.launch {
-                        state.animateScrollToItem(0)
-                    }
-                }
-            }
-        } else {
-            LazyRow(
-                modifier = Modifier.fillMaxHeight(),
-                verticalAlignment = Alignment.CenterVertically,
-                contentPadding = PaddingValues(10.dp),
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-//                itemsIndexed(items = imageList ) { index, item ->
-//                    ImageList(
-//                        modifier = Modifier,
-//                        imageList = mutableListOf(item).apply {
-//                            this[0] = item
-//                        }
-//                    )
-//                }
-                item() {
-                    ImageList(imageList = imageList)
-                }
-            }
+    } else {
+        Row(
+            modifier = Modifier.fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ImageList(imageList = imageList)
         }
     }
 
