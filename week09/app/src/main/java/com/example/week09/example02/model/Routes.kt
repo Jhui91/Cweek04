5ad0fff17c27cd3c47fda007d86738be1df8b46f
Jhui91 <jinso0911@naver.com>
2025-05-01 13:36:30 +0900

    Update Routes.kt

--- week09/app/src/main/java/com/example/week09/example02/model/Routes.kt
+++ week09/app/src/main/java/com/example/week09/example02/model/Routes.kt
@@ -1,8 +1,8 @@
 package com.example.week09.example02.model
 
-sealed class Routes (val route: String) {
+sealed class Routes (val route: String, val isRoot: Boolean = true) {
     object Home : Routes("Home")
     object Contacts : Routes("Contacts")
     object Favorites : Routes("Favorites")
-    object AddContacts : Routes("AddContacts")
+    object AddContacts : Routes("AddContacts", isRoot = false)
 }
