3a2476caab3cde3566c3600a98b01d082a8516c3
Jhui91 <jinso0911@naver.com>
2025-05-01 13:19:21 +0900

    Update BarItem.kt

--- week09/app/src/main/java/com/example/week09/example02/model/BarItem.kt
+++ week09/app/src/main/java/com/example/week09/example02/model/BarItem.kt
@@ -2,5 +2,8 @@ package com.example.week09.example02.model
 
 import androidx.compose.ui.graphics.vector.ImageVector
 
-data class BarItem (val title :String, val selectIcon: ImageVector, val onSelectedIcon :ImageVector, val route:String)
+data class BarItem (val title :String, 
+                    val selectIcon: ImageVector,
+                    val onSelectedIcon :ImageVector,
+                    val route:String)
 
