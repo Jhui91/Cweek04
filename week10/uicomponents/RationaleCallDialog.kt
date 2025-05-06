4abc838f6b983ad16c606044e297a975f14aaf8f
Jhui91 <jinso0911@naver.com>
2025-05-06 13:59:24 +0900

    Update RationaleCallDialog.kt

--- week10/uicomponents/RationaleCallDialog.kt
+++ week10/uicomponents/RationaleCallDialog.kt
@@ -6,14 +6,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun RationaleDialog(
+fun RationaleCallDialog(
     onDismiss: () -> Unit,
     onConfirm: () -> Unit,
 ) {
     AlertDialog(
         onDismissRequest = onDismiss,
         title = { Text("권한 확인요청") },
-        text = { Text("CALL_PHONE과 CAMERA 권한이 승인되어야 합니다.") },
+        text = { Text("전화 기능을 사용하려면 CALL_PHONE 권한이 승인되어야 합니다.") },
         confirmButton = {
             Button(onClick = onConfirm) {
                 Text("권한승인")
